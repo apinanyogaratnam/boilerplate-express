f4387988185a0230c7390687c83d2a8699b6dcd3
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-09-29 16:14:50 +0000

    attempted changes to display json file with uppercase

--- myApp.js
+++ myApp.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+const mySecret = process.env['MESSAGE_STYLE']
+
 
 console.log("Hello World");
 
@@ -11,6 +13,16 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", function(req, res) {
     const absolutePath = __dirname + "/views/index.html";
     res.sendFile(absolutePath);
+});
+
+app.get("/json", function(req, res) {
+    var response;
+    if (mySecret === "uppercase") {
+        response = "Hello json".toUpperCase();
+    } else {
+        response = "Hello json";
+    }
+    res.json({"message": response});
 });
 
 
