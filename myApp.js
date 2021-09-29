d7edb0506e836c4c9d6899fcf4ce87a8abab4fad
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-09-29 15:43:37 +0000

    added css to app from express

--- myApp.js
+++ myApp.js
@@ -6,6 +6,7 @@ console.log("Hello World");
 // app.get("/", function(req, res) {
 //     res.send("Hello Express");
 // });
+app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", function(req, res) {
     const absolutePath = __dirname + "/views/index.html";
