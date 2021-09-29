680bc271abf8f55fd76ad1011a090d3f0038a127
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-09-29 14:07:20 -0400

    completed middleware section of logging req data

--- myApp.js
+++ myApp.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var app = express();
-const mySecret = process.env['MESSAGE_STYLE']
-
-
-console.log("Hello World");
+process.env.MESSAGE_STYLE = 'uppercase';
 
 // app.get("/", function(req, res) {
 //     res.send("Hello Express");
 // });
+
 app.use("/public", express.static(__dirname + "/public"));
+app.use(function(req, res, next) {
+    console.log(req.method + " " + req.path + " - " + req.ip);
+    next();
+});
 
 app.get("/", function(req, res) {
     const absolutePath = __dirname + "/views/index.html";
@@ -17,7 +19,7 @@ app.get("/", function(req, res) {
 
 app.get("/json", function(req, res) {
     var response;
-    if (mySecret === "uppercase") {
+    if (process.env.MESSAGE_STYLE == "uppercase") {
         response = "Hello json".toUpperCase();
     } else {
         response = "Hello json";
